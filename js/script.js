3b62831d00adc27a9161ed35921217d5ff7694c4
Alan Fidelino <afidedev26@gmail.com>
2018-12-12 14:17:36 +1100

    Finish first JS project

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-alert("Welcome!");
+var cards = document.getElementsByClassName('card');
+
+for (var i = 0; i < cards.length; i++) {
+  var button = cards[i].querySelector('.card-button');
+
+
+  button.addEventListener('click', function (e) {
+    e.preventDefault();
+    var currentButton = e.target;
+    var currentCard = currentButton.parentNode;
+    var currentAnswer = currentCard.querySelector('.card-answer');
+    // console.log(currentButton.innerHTML);
+    // console.log(currentAnswer.innerHTML);
+    // console.log(currentCard);
+
+    if (currentButton.innerHTML == 'Show') {
+      currentButton.innerHTML = 'Hide';
+      currentAnswer.style.display = "block";
+    } else if (currentButton.innerHTML == 'Hide') {
+      currentButton.innerHTML = 'Show';
+      currentAnswer.style.display = "none";
+    }
+  })
+}
